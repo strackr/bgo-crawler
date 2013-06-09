305f6eaed65b746662ae64c7174281eec418e0ab
Robert Strack <robert.strack@gmail.com>
2013-06-09 00:27:01 -0400

    minor log parser refactoring

--- bgo-crawler/bgo/parser.py
+++ bgo-crawler/bgo/parser.py
@@ -13,33 +13,34 @@ class LogParser:
         self.user = user
         self.password = password
         self.game = game
+
+        self.driver = webdriver.Firefox()
+        self.wait = ui.WebDriverWait(self.driver, 10)
     
     def extract_logs(self):
-        driver = webdriver.Firefox()
-        wait = ui.WebDriverWait(driver, 10)
-         
-        driver.get(self.login_url)
-        elem = driver.find_element_by_id('identifiant')
+        self.driver.get(self.login_url)
+        elem = self.driver.find_element_by_id('identifiant')
         elem.send_keys(self.user)
-        elem = driver.find_element_by_id('mot_de_passe')
+        elem = self.driver.find_element_by_id('mot_de_passe')
         elem.send_keys(self.password)
         elem.send_keys(Keys.RETURN)
-        wait.until(lambda driver: driver.find_element_by_id('contenu'))
+        self.wait.until(lambda driver: driver.find_element_by_id('contenu'))
          
-        driver.get(self.game_url.format(self.game, 1))
-        wait.until(lambda driver: driver.find_element_by_id('contenu'))
+        self.driver.get(self.game_url.format(self.game, 1))
+        self.wait.until(lambda driver: driver.find_element_by_id('contenu'))
          
-        pages = sorted(set([u'1'] + re.findall(r'&amp;pg=(\d+)&amp;', driver.page_source)), key=lambda x: int(x))
+        pages = sorted(set([u'1'] + re.findall(r'&amp;pg=(\d+)&amp;', self.driver.page_source)), key=lambda x: int(x))
         entries = []        
         for page in pages:
-            entries += self.extract_page_logs(driver, page)
+            entries += self.extract_page_logs(page)
         
-        driver.close()
+        self.driver.close()
         entries.reverse()
         return entries
 
-    def extract_page_logs(self, driver, page_id):
-        driver.get(self.game_url.format(self.game, page_id))
+    def extract_page_logs(self, page_id):
+        self.driver.get(self.game_url.format(self.game, page_id))
+        self.wait.until(lambda driver: driver.find_element_by_id('contenu'))
         
         pattern = '<tr>' \
                 '<td class="ligneJournalC"><p class="texte">(.+?)</p></td>' \
@@ -48,4 +49,4 @@ class LogParser:
                 '<td class="ligneJournalC"><p class="texte">(\d+)</p></td>' \
                 '<td class="ligneJournal"><p class="titre3">(.+?)</p><p class="texte">(.*?)</p></td>' \
                 '</tr>'
-        return re.findall(pattern, driver.page_source, re.DOTALL)
+        return re.findall(pattern, self.driver.page_source, re.DOTALL)
